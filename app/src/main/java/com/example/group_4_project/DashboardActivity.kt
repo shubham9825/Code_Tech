e09a5853a4ba0e6fe348a8df002a64ae2731bf5b
Jai Maurya <jaimaurya123456@gmail.com>
2024-03-17 10:31:59 -0400

    Added the necessary images

---
+++ app/src/main/java/com/example/group_4_project/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.example.group_4_project
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class DashboardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.dashboard)
+    }
+}
