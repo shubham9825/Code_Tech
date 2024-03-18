251c095f39626f9ef93fd0c27d5090c749b8b3ae
Jai Maurya <jaimaurya123456@gmail.com>
2024-03-17 21:03:28 -0400

    Checking for the dashboard files

---
+++ app/src/main/java/com/example/group_4_project/Dashboard.kt
@@ -0,0 +1,9 @@
+package com.example.group_4_project
+
+
+data class Dashboard(
+    var companyName: String="",
+    var jobTitle: String="",
+    var description: String="",
+    var companyImageUrl: String=""
+)
--- app/src/main/java/com/example/group_4_project/DashboardActivity.kt
+++ app/src/main/java/com/example/group_4_project/DashboardActivity.kt
@@ -1,23 +1,34 @@
 package com.example.group_4_project
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
 class DashboardActivity : AppCompatActivity() {
+
     private lateinit var bottomNavigationView: BottomNavigationView
     private lateinit var logoutButton: Button
     private lateinit var auth: FirebaseAuth
-
+    private lateinit var databaseReference: DatabaseReference
+    private var rView: RecyclerView? = null
+    private var adapter: DashboardAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard)
+
         auth = FirebaseAuth.getInstance()
-        logoutButton = findViewById(R.id.btnLogout);
+        databaseReference = FirebaseDatabase.getInstance().reference.child("jobs")
+
+        logoutButton = findViewById(R.id.btnLogout)
         logoutButton.setOnClickListener {
             signOut()
         }
@@ -26,10 +37,9 @@ class DashboardActivity : AppCompatActivity() {
         bottomNavigationView.setOnItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.navigation_dashboard -> {
-                    startActivity(Intent(this, DashboardActivity::class.java))
+
                     true
                 }
-
                 R.id.navigation_candidate -> {
                     startActivity(Intent(this, CandidateActivity::class.java))
                     true
@@ -37,7 +47,19 @@ class DashboardActivity : AppCompatActivity() {
                 else -> false
             }
         }
+
+        // RecyclerView setup
+        rView = findViewById(R.id.dashboardRecyclerView)
+        rView?.layoutManager = LinearLayoutManager(this)
+        val options = FirebaseRecyclerOptions.Builder<Dashboard>()
+            .setQuery(databaseReference, Dashboard::class.java)
+            .build()
+        adapter = DashboardAdapter(options)
+        rView?.adapter = adapter
     }
+
+
+
     private fun signOut() {
         auth.signOut()
         Toast.makeText(this@DashboardActivity, "Logged out successfully", Toast.LENGTH_SHORT).show()
